3a0fd26997512570e62bc84f04c5a4a4313e1e3a
Gavan Browne <gavan.browne@gmail.com>
2024-11-14 19:37:29 +0000

    finalize code to calc determinant of 3x3 matrix
    Moved code to calculate 3x3 determinant from main to a static method. Updated code so that it correctly calculates and prints the determinant of a 3x3 matrix.

--- src/determinant3x3matrix/Determinant3x3Matrix.java
+++ src/determinant3x3matrix/Determinant3x3Matrix.java
@@ -4,8 +4,6 @@
  */
 package determinant3x3matrix;
 
-import java.util.Arrays;
-
 /**
  *
  * @author gavan
@@ -16,13 +14,88 @@ public class Determinant3x3Matrix {
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     }
 
+    private static void calc3x3Determinant(int[][] matrix) {
+        int numElements = 9; // 9 elements in the matrix
+        int sideLength = 3; // 3x3 matrix
+        int determinant = 0; // store the determinant sum as we calculate it
+
+        // for every all elements in first row of matrix
+        for (int i = 0; i < sideLength; i++) {
+
+            /*
+            If we flatten the matrix into a 1D array, then we can calculate the
+            row and column of the i-th element of the 1D array
+             */
+            int row = Math.floorDiv(i, sideLength);
+            int col = i % sideLength;
+
+            // we get the scalar in our original array using the row and col we
+            // calculated
+            int scalar = matrix[row][col];
+
+            /* we use minorIndex for the following:
+            i) To keep track of our position in the matrix of minors as we fill
+            in the values
+            ii) To terminate our inner for loop once minorMatrix is full
+             */
+            int minorIndex = 0;
+            int minorMatrix[][] = new int[2][2];
+
+            // this loop constructs the matrix of minors for each element a i
+            for (int j = 0; j < numElements; j++) {
+                // the matrix of minors only has 4 elements
+                if (minorIndex > 3) {
+                    break;
+                }
+
+                // the element itself can't be in the matrix of minors
+                if (j == i) {
+                    continue;
+                }
+
+                // convert our 1D index, j, to a 2D row and col
+                int currRow = Math.floorDiv(j, sideLength);
+                int currCol = j % sideLength;
+
+                // ignore the row and column that contain our element at index i
+                if (currRow == row || currCol == col) {
+                    continue;
+                }
+
+                // we convert the 1D value of minorIndex to the
+                // 2D row and column equivalent
+                int minorMatrixRow = Math.floorDiv(minorIndex, minorMatrix.length);
+                int minorMatrixCol = minorIndex % minorMatrix.length;
+
+                // add the element to the matrix of minors
+                minorMatrix[minorMatrixRow][minorMatrixCol] = matrix[currRow][currCol];
+
+                // increment minorIndex, ready to add the next element
+                minorIndex++;
+
+            }
+
+            // we use i to determine the sign of the minor
+            if (i % 2 == 0) {
+                determinant += calc2x2Determinant(minorMatrix) * scalar;
+            } else {
+                determinant += -(calc2x2Determinant(minorMatrix) * scalar);
+            }
+        }
+        System.out.println("determinant of 3x3 matrix: " + determinant);
+
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         int matrix[][] = {{1, 2, 3}, {4, 1, 5}, {6, 0, 2}};
-        int numElements = 9;
-        int sideLength = 3;
+        int anotherMatrix[][] = {{2, -3, 1}, {5, -1, 2}, {3, 2, -1}};
+
+        calc3x3Determinant(anotherMatrix);
+        calc3x3Determinant(matrix);
+
 
         /* steps to find determinant of 3x3
         // flattening the matrix would make this easier to iterate?
@@ -38,42 +111,5 @@ public class Determinant3x3Matrix {
         ii)     determine the row and col of the current element
         iii)
          */
-        // for every element in the matrix - correction, only need to do first row
-        for (int i = 0; i < sideLength; i++) {
-
-            int row = Math.floorDiv(i, sideLength);
-            int col = i % sideLength;
-            int scalar = matrix[row][col];
-            int minorIndex = 0;
-            int minorMatrix[][] = new int[2][2];
-            for (int j = 0; j < numElements; j++) {
-                if (minorIndex > 3) {
-                    break;
-                }
-
-                // for every element in the matrix, construct the 2x2 matrix
-                // the element itself can't be in the matrix of minors
-                if (j == i) {
-                    continue;
-                }
-
-                int currRow = Math.floorDiv(j, sideLength);
-
-                int currCol = j % sideLength;
-                if (currRow == row || currCol == col) {
-                    continue;
-                }
-                // add the element to the matrix of minors
-
-                int minorMatrixRow = Math.floorDiv(minorIndex, minorMatrix.length);
-                int minorMatrixCol = minorIndex % minorMatrix.length;
-                minorMatrix[minorMatrixRow][minorMatrixCol] = matrix[currRow][currCol];
-                minorIndex++;
-
-            }
-            System.out.println("scalar: " + scalar);
-            System.out.println(Arrays.deepToString(minorMatrix));
-            System.out.println("product of scalar by det(minorMatrix): " + calc2x2Determinant(minorMatrix) * scalar);
-        }
     }
 }
